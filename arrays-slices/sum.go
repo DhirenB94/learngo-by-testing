7e5f95fd09f8e88ff0a73417ccd804003ef3edb6
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-05 18:48:18 +0000

    refactoring using range

--- arrays-slices/sum.go
+++ arrays-slices/sum.go
@@ -4,9 +4,14 @@ func Sum(numbers[5]int) int {
 
 	sum := 0
 
-	for i:=0; i< len(numbers); i++ {
-		sum = sum + numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 
 	return sum
 }
+
+// range
+// --lets you iterate over an array
+// -- on each iteration, range returns 2 values, --> the index position and the value
+// -- by using _, we are choosing to ignore the index position
