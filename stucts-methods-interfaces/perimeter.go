4d31c39d0d6061c9314694267a9542a402b0178e
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-09 14:41:02 +0000

    fixing the functions to pas the test

--- stucts-methods-interfaces/perimeter.go
+++ stucts-methods-interfaces/perimeter.go
@@ -1,12 +1,16 @@
 package perimeter
 
-func Perimeter(width float64, height float64) float64 {
-	return width *2 + height*2
+func Perimeter(rectanglePerim Rectangle) float64 {
+	return 2 * (rectanglePerim.width + rectanglePerim.height) //access the structs fields
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+func Area(rectangleArea Rectangle) float64 {
+	return rectangleArea.width * rectangleArea.height
 }
+
+
+
+
 
 //Structs
 //named collection of fields where you can store data
