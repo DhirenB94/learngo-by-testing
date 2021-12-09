f9699b125b9cd1c6fa3585e719d8ebcf19043953
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-09 16:44:20 +0000

    explanation as to why the interface works

--- stucts-methods-interfaces/perimeter.go
+++ stucts-methods-interfaces/perimeter.go
@@ -28,8 +28,7 @@ type Shape interface {
 	 Area() float64
 }
 
-
-
+//Rextangle and Circle have methods called Area() that return a float64, so it satisfies the Shape interface
 
 
 //Structs
@@ -37,13 +36,13 @@ type Shape interface {
 
 
 //Methods
-/*
-So far we have only been writing functions but we have been using some methods.
-When we call t.Errorf we are calling the method Errorf on the instance of our t (testing.T).
-A method is a function with a receiver. A method declaration binds an identifier, the method name, to a method, and associates the method with the receiver's base type.
-Methods are very similar to functions but they are called by invoking them on an instance of a particular type. Where you can just call functions wherever you like, such as Area(rectangle) you can only call methods on "things".
 
-The syntax for declaring methods is almost the same as functions and that's because they're so similar. The only difference is the syntax of the method receiver - func (receiverName ReceiverType) MethodName(args) -
-When your method is called on a variable of that type, you get your reference to its data via the receiverName variable. In many other programming languages this is done implicitly and you access the receiver via this.
-It is a convention in Go to have the receiver variable be the first letter of the type.*/
+//When we call t.Errorf() we are calling the method Errorf on the instance of our t (testing.T).
+//A method is a function with a receiver.
+//Where you can just call functions wherever you like, such as Area(rectangle) you can only call methods on "things".
+
+//Syntax --> func (receiverName ReceiverType) MethodName(args) -
+//convention in Go to have the receiver variable be the first letter of the type.*/
+
+
 
