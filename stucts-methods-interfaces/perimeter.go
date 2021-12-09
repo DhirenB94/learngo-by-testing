9df36224bb679f4373cd03ead2a271afb0a6c8d9
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-09 15:31:30 +0000

    showing the current error and some info about methods

--- stucts-methods-interfaces/perimeter.go
+++ stucts-methods-interfaces/perimeter.go
@@ -7,9 +7,7 @@ func Perimeter(rectanglePerim Rectangle) float64 {
 func Area(rectangleArea Rectangle) float64 {
 	return rectangleArea.width * rectangleArea.height
 }
-
-
-
+// current error : cannot use circle (type Circle) as type Rectangle in argument to Area
 
 
 //Structs
@@ -24,4 +22,11 @@ type Circle struct {
 	radius float64
 }
 
+//Methods
+
+/*
+So far we have only been writing functions but we have been using some methods. When we call t.Errorf we are calling the method Errorf on the instance of our t (testing.T).
+A method is a function with a receiver. A method declaration binds an identifier, the method name, to a method, and associates the method with the receiver's base type.
+Methods are very similar to functions but they are called by invoking them on an instance of a particular type. Where you can just call functions wherever you like, such as Area(rectangle) you can only call methods on "things".
+*/
 
