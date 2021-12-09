d707b55f5138a8ae3af306ca686adc756a0ee63b
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-09 14:19:00 +0000

    minimal amount of code for test to run

--- stucts-methods-interfaces/perimeter.go
+++ stucts-methods-interfaces/perimeter.go
@@ -3,3 +3,7 @@ package perimeter
 func Perimeter(width float64, height float64) float64 {
 	return width *2 + height*2
 }
+
+func AreaOfRectangle(width float64, height float64) float64 {
+	return 0
+}
