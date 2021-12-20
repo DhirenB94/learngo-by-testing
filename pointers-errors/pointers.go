1eb16b2901a09c42d96b13e77a1ebc933cc96e35
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-20 20:59:34 +0000

    demonstrating 2 different addresses in test and in func, hence failing test

--- pointers-errors/pointers.go
+++ pointers-errors/pointers.go
@@ -1,11 +1,14 @@
 package pointers
 
+import "fmt"
+
 type Wallet struct {
 	balance int
 }
 
 
 func (w Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %v\n", &w.balance)
 	w.balance += amount
 }
 
--- pointers-errors/pointers_test.go
+++ pointers-errors/pointers_test.go
@@ -1,14 +1,22 @@
 package pointers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
-	wallet := wallet {}
+	wallet := Wallet{}
 
 	wallet.Deposit(10)
 
 	got := wallet.Balance()
+	fmt.Printf("address of balance in test is %v\n", &wallet.balance)
 	want := 10
+//the addresses of the balance field is different here in the test (accessing from Wallet struct
+//compared to the balance field used in the function (accessing from w Wallet struct)
+//this is because it uses a copy n the function
+
 
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
