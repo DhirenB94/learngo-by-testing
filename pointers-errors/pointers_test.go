59bb37d5f3676ed9340d31c361c09f7c76b9f31c
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-20 21:22:25 +0000

    updating test format strings to use String() instead, and deliberately faling to see

--- pointers-errors/pointers_test.go
+++ pointers-errors/pointers_test.go
@@ -10,10 +10,10 @@ func TestWallet(t *testing.T) {
 	wallet.Deposit(Bitcoin (10))
 
 	got := wallet.Balance()
-	want := Bitcoin(10)
+	want := Bitcoin(20)
 
 
 	if got != want {
-		t.Errorf("got %d, want %d", got, want)
+		t.Errorf("got %s, want %s", got, want)
 	}
 }
