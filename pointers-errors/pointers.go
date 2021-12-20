7985d3800806d856473883d4d1f6eb1b84e9746c
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-20 20:50:59 +0000

    writing a test that LOOKS like it should pass

--- pointers-errors/pointers.go
+++ pointers-errors/pointers.go
@@ -1,12 +1,23 @@
 package pointers
 
-type Wallet struct {}
+type Wallet struct {
+	balance int
+}
 
 
 func (w Wallet) Deposit(amount int) {
-	
+	w.balance += amount
 }
 
 func (w Wallet) Balance() int {
-	return 0
+	return w.balance
 }
+
+//You would think this would return a passing test
+//as you are depositing the amount variable (10) to the balance
+//then the balance method should return the current state of it
+
+//BUT NO
+// In Go, when you call a function or a method, the arguements are copied!
+//so when we call func(w Wallet)Deposit(amount int) --> the w is a copy of whatever we called the method from
+
