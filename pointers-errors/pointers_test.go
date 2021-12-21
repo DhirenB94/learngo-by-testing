252aaf2894c361b38ccb41464dbffea0b588a50f
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-21 18:28:37 +0000

    writing a non compiling test for withdrawing more than the balance

--- pointers-errors/pointers_test.go
+++ pointers-errors/pointers_test.go
@@ -31,4 +31,27 @@ func TestWallet(t *testing.T) {
 		wallet.Withdraw(Bitcoin(5))
 		assertBalance(t, wallet, Bitcoin(15))
 	})
+
+	//Returning error when withdrawal exceeds balance
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(100)
+
+		wallet := Wallet{balance: startingBalance}
+		err := wallet.Withdraw(Bitcoin(200))
+
+
+		assertBalance(t, wallet, startingBalance)
+
+		if err == nil {
+			t.Errorf("Wanted an error but didnt get one")
+		}
+
+		// we want to return an error if you try and withdraw more than you have, and the balance should stay the same
+		//we then check an error has returned by failing the test if it is nil
+		//Errors can be nil because the return type of Withdraw will be error, which is an interface.
+		//functions that take arguments or return values that are interfaces, they can be nillable.
+
+		// Current error
+		// ./pointers_test.go:40:25: wallet.Withdraw(Bitcoin(200)) used as value
+	})
 }
