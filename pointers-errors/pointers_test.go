64edddc1962d2392f7fb1853ab8eb4da64bdb04f
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-22 16:41:05 +0000

    unchecked errors

--- pointers-errors/pointers_test.go
+++ pointers-errors/pointers_test.go
@@ -5,26 +5,6 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-
-		if got != want {
-			t.Errorf("got %s, want %s", got, want)
-		}
-	}
-
-	assertError := func(t testing.TB, got error, want error) {
-		t.Helper()
-		if got == nil {
-			t.Fatalf("did not get an error but wanted one")
-		}
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	}
-
 	t.Run("Deposit test", func(t *testing.T) {
 	wallet := Wallet{}
 
@@ -41,8 +21,12 @@ func TestWallet(t *testing.T) {
 		wallet.Withdraw(Bitcoin(5))
 		assertBalance(t, wallet, Bitcoin(15))
 	})
+	//running errcheck . gives us an error with line 21
+	//we have not checked the error being returned here
+	//it corresponds to our normal withdraw scenario (i.e enough funds for withdraw to proceed)
+	//because we have not checked that if the Withdraw is successful, that an error is not returned
 
-	//Returning error when withdrawal exceeds balance
+
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
 		startingBalance := Bitcoin(100)
 
@@ -53,7 +37,24 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, wallet, startingBalance)
 
 	})
+}
+func assertBalance (t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
 
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
 }
 
+func assertError (t testing.TB, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatalf("did not get an error but wanted one")
+	}
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
 
