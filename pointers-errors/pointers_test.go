cd4f0ff82fcb68a210dd87aad14151579fc03512
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-22 17:22:46 +0000

    resolving unhecked errors

--- pointers-errors/pointers_test.go
+++ pointers-errors/pointers_test.go
@@ -18,14 +18,10 @@ func TestWallet(t *testing.T) {
 			balance: 20,
 		}
 
-		wallet.Withdraw(Bitcoin(5))
+		err := wallet.Withdraw(Bitcoin(5))
 		assertBalance(t, wallet, Bitcoin(15))
+		assertNoError(t, err)
 	})
-	//running errcheck . gives us an error with line 21
-	//we have not checked the error being returned here
-	//it corresponds to our normal withdraw scenario (i.e enough funds for withdraw to proceed)
-	//because we have not checked that if the Withdraw is successful, that an error is not returned
-
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
 		startingBalance := Bitcoin(100)
@@ -57,4 +53,11 @@ func assertError (t testing.TB, got error, want error) {
 		t.Errorf("got %q, want %q", got, want)
 	}
 }
+// Check if withdraw is successful, an error is NOT returned
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
 
+	if got != nil {
+		t.Fatalf("did not get an error but wanted one")
+	}
+}
