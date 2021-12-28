8916567b70726de0d9b10bbd353fa3fe630192da
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-28 16:57:48 +0000

    writing enough code to make the test pass, utilising map returning 2 values

--- maps/maps.go
+++ maps/maps.go
@@ -1,11 +1,18 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-
 func (d Dictionary) Search(word string) (string, error) {
-	return d[word], nil
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("the word you are looking for can not be found")
+	}
+
+	return definition, nil
 }
 
-//it fails in the unknown word test because you are searching "unkwown"key in dictionary
-//dictionary only has "test"key --> so it returns nil and therefore t.fatal
+//In order to make this pass, we are using an interesting property of the map lookup. It can return 2 values.
+//The second value is a boolean which indicates if the key was found successfully.
+//This property allows us to differentiate between a word that doesn't exist and a word that just doesn't have a definition.
