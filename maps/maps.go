38a22fc04358a94ebc64a4c4a44f4b314ec85ee9
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-28 16:55:39 +0000

    explaining searching a work that doesnt exist in the map

--- maps/maps.go
+++ maps/maps.go
@@ -2,8 +2,10 @@ package maps
 
 type Dictionary map[string]string
 
+
 func (d Dictionary) Search(word string) (string, error) {
 	return d[word], nil
 }
 
-//as we are returning nil for the error value our test instructs it to t.fatal with "expected to get an error"
+//it fails in the unknown word test because you are searching "unkwown"key in dictionary
+//dictionary only has "test"key --> so it returns nil and therefore t.fatal
