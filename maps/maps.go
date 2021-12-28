fffab22e6f39b4d16745d4b759e8f713239284f2
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-28 15:46:30 +0000

    writing initial non compiling test

---
+++ maps/maps.go
@@ -0,0 +1,8 @@
+package maps
+
+//MAPS
+//allow you to store items in a key:value
+
+//syntax --> map[type1]type2 {}
+//map keyword followed by 2 types
+//1st type is the key type and the 2nd the value type
