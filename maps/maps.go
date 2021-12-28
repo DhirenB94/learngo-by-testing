1a81dc2f6f109e384502365250b7233faebf97b9
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-28 15:53:59 +0000

    minimal amount of code for test to pass

--- maps/maps.go
+++ maps/maps.go
@@ -1,12 +1,8 @@
 package maps
 
-//MAPS
-//allow you to store items in a key:value
-
-//syntax --> map[type1]type2 {}
-//map keyword followed by 2 types
-//1st type is the key type and the 2nd the value type
 
 func Search(dictionary map[string]string, word string) string{
-	return ""
+	return dictionary[word]
 }
+//getting a value out of map is same as getting one from an array
+//map[key]
