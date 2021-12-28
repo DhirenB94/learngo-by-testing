101738f27fa05f0ee65f532c1644ef296241e2b5
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-28 16:33:22 +0000

    initial non compiling test for an unkmown word

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -5,10 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test" : "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want )
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "the word you are looking for can not be found"
+
+		if err == nil {
+			t.Fatalf("expected to get an error")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 
 }
 
@@ -20,6 +33,11 @@ func assertStrings(t testing.TB, got, want string)  {
 	}
 }
 
+//what happens if the word we search for is not in the map - we want to communicate this to the user
+//we return a 2nd argument which is an Error type
+//Errors can be converted to a string with the .Error() method, which we do when passing it to the assertion
+//We are also protecting assertStrings with if to ensure we don't call .Error() on nil.
 
-//we have started using the Dictionary type (which we need to define in dictionary.go
-//and then called the search method on the dictionary instance
+
+//current test output
+// ./maps_test.go:16:10: assignment mismatch: 2 variables but dictionary.Search returns 1 value
