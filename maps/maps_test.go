2f4faaa27a1d3d4f59f0d57bdad2f354b01434fa
Dhiren Brahmbhatt <dhiren.brahmbhattl@saltpay.co>
2021-12-30 14:32:41 +0000

    refactoring test, making assertDefinition func

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -35,29 +35,28 @@ func assertError(t testing.TB, got, want error) {
 		t.Errorf("got error %q, want %q", got, want)
 	}
 }
-//new test to add words
-//utilise our search method to make validation of our dictionary easier
 
 func TestAdd(t *testing.T) {
 	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
+	word:= "test"
+	definition := "this is just a test"
+	dictionary.Add(word, definition)
 
-	want := "this is just a test"
+	assertDefinition(t, dictionary, word, definition)
+
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string)  {
+	t.Helper()
+
 	got, err := dictionary.Search("test")
 
 	if err != nil {
 		t.Fatal("should find added word:", err)
 	}
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	if got != definition {
+		t.Errorf("got %q, want %q", got, definition)
 	}
 }
-
-//creating an empty map
-//calling add method (which currently does nothing)
-//then calling the search method which returns 2 values - string and error
-// as Add() doesnt do anyhthing, you are searching for "test" in an empty map so it will follow the unkown word path
-//this will return empty string and ErrNotFound - assigned to got and err
-//err does not qual nil it equals ErrNotFound so fatals with message and the err
 
