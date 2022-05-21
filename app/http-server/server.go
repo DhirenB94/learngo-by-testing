6b82c8c1a290f3aea95ee75ba70e3aa401bdfc92
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2022-05-21 18:57:36 +0100

    http-server : architecture change, introducing a playerstore interface

--- app/http-server/server.go
+++ app/http-server/server.go
@@ -6,17 +6,22 @@ import (
 	"strings"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	fmt.Fprint(w, GetPlayerScore(player))
+type PlayerStore interface {
+	GetPlayerScore(name string) int
 }
 
-func GetPlayerScore(name string) string {
-	if name == "Pedro" {
-		return "20"
-	}
-	if name == "Floyd" {
-		return "10"
-	}
-	return ""
+//our server should not know the score, so we use our GetPlayerScorefunc into an interface
+
+//for our PlayerServer to use the PlayerStore we need to change it into a struct
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+//Now to implement the Handler interface (with the serveHttp method), we add a method to our new struct with the existing handler code we had
+//this method is the same as the method on the handler interface
+
+func (ps *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprint(w, ps.store.GetPlayerScore(player))
 }
