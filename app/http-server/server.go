5baa86fb4a9455e5816ed1d8247c006ae773b199
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2022-05-21 21:12:45 +0100

    http-server : minimal amount of code to make missing player test pass

--- app/http-server/server.go
+++ app/http-server/server.go
@@ -16,5 +16,6 @@ type PlayerServer struct {
 
 func (ps *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, ps.Store.GetPlayerScore(player))
 }
