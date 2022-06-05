0a996d74a68310e2c7824fd73e3b3e6030a8cc13
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2022-06-05 17:08:53 +0100

    Dependency Injection: initial test

---
+++ dependency-injection/dependency_injection_test.go
@@ -0,0 +1,22 @@
+package dependency
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	//buffer type from the bytes package, implements the Writer interface
+	//so we use it to send in as our Writer
+
+	//then we can check what was written to it after calling Greet
+	Greet(&buffer, "Ziggy")
+
+	got := buffer.String()
+	want := "Hello, Ziggy"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
