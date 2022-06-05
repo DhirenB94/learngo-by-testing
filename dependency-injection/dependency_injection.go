97245d29d5ac9b06ac82d30058664562ffb550c4
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2022-06-05 17:18:35 +0100

    Dependency Injection:passing test

--- dependency-injection/dependency_injection.go
+++ dependency-injection/dependency_injection.go
@@ -1,1 +1,14 @@
 package dependency
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func Greet(writer *bytes.Buffer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+//changed fmt.Printf to --> fmt.Fprintf
+//this is because fmt.Printf sends to stdout
+//whereas fmt.Fprintf, will send the string to a Writer (anything that has the Write method)
