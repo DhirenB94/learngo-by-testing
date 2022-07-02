eff5e58047d2b55eacac3df8dc850859d94b3916
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2022-07-02 15:11:11 +0100

    Mocking: refactor the tests and remove old sleeper

--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -6,18 +6,6 @@ import (
 	"testing"
 )
 
-type SpySleeper struct {
-	Calls int
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-
-//Currently our test asserts that the sleep() is called 3 times
-//but this does not cover WHEN the sleep is called
-//eg in the case of an initial 3 sec sleep THEN the countdown beginning this would still pass
-
 const sleep = "sleep"
 const write = "write"
 
@@ -34,32 +22,27 @@ func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 }
 
 //Our SpyCountdownOperations implements both io.Writer and Sleeper, recording every call into one slice.
-//In this test we're only concerned about the order of operations, so just recording them as list of named operations is sufficient.
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
 
-	Countdown(buffer, spySleeper)
+	t.Run("print 3 to 1 then Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpyCountdownOperations{}
 
-	got := buffer.String()
-	want := `3
+		Countdown(buffer, spySleeper)
+
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
-
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
-
-	//want to have called the sleep() 3 times
-	if spySleeper.Calls != 3 {
-		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 
 	t.Run("test order of countdown", func(t *testing.T) {
 		spySleepPrinter := &SpyCountdownOperations{}
-
 		Countdown(spySleepPrinter, spySleepPrinter)
 
 		want := []string{"write", "sleep", "write", "sleep", "write", "sleep", "write"}
